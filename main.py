5e754ccebf11d4f5773dc6d88fef60a6cffcd292
arfelious <arfelious@proton.me>
2024-05-10 03:02:00 +0300

    .

--- main.py
+++ main.py
@@ -1,39 +1,59 @@
 import reverse_geocoder as rg
 import pandas as pd
 import matplotlib.pyplot as plt
+import bar_chart_race as bcr
+import plotly
 import h3
 import os
+import datetime
 import math
 if __name__ == '__main__':
-    test=True
-    dosyalar=os.listdir("./data/map")
+    def oranla(sorunsuz,sorunlu):
+        return max(0,(sorunsuz-1)/max(1,sorunsuz+sorunlu))
+    test=True   
+    dosyalar=sorted(os.listdir("./data/map"))
     veriler=[]
-    miktar=min(len(dosyalar),3) if test else len(dosyalar)
+    veri_gunler = []
+    miktar=min(len(dosyalar),50) if test else len(dosyalar)
     for i in range(miktar):
+        veri_gunler.append(datetime.datetime.strptime(dosyalar[i].split(".csv")[0],"%Y-%m-%d"))
         ayristirilan=[]
         koordinatlar=[]
         df=pd.read_csv("./data/map/"+dosyalar[i])
         df_miktar=min(len(df),10000) if test else len(df)
         for j in range(df_miktar):
-            if j%1000==0:print(j,df_miktar,j/df_miktar)
+            if j%1000==0:print(i,miktar,i/miktar,j,df_miktar,j/df_miktar)
             guncel=df.loc[j]
             koordinatlar.append(h3.h3_to_geo(guncel.hex))
             ayristirilan.append([int(guncel.count_good_aircraft),int(guncel.count_bad_aircraft)])
         ulkeler=rg.search(koordinatlar,verbose=False)
         veriler.append([[ulkeler[i]["cc"],ulkeler[i]["admin1"],ayristirilan[i][0],ayristirilan[i][1]] for i in range(df_miktar)])
         ulke_basina={}
-    for i in veriler:
-        for j in i:
+    gunluk_ulke_sayac=[{} for i in range(miktar)]
+    for i in range(miktar):
+        guncel_veri=veriler[i]
+        for j in guncel_veri:
             if not j[0] in ulke_basina:ulke_basina[j[0]]={}
             if not j[1] in ulke_basina[j[0]]: ulke_basina[j[0]][j[1]]=[0,0]
             ulke_basina[j[0]][j[1]]=[ulke_basina[j[0]][j[1]][0]+j[2],ulke_basina[j[0]][j[1]][1]+j[3]]
+            if not j[0] in gunluk_ulke_sayac[i]:gunluk_ulke_sayac[i][j[0]]=[0,0]
+            gunluk_ulke_sayac[i][j[0]]=[gunluk_ulke_sayac[i][j[0]][0]+j[2],gunluk_ulke_sayac[i][j[0]][1]+j[3]]
     ulke_sayac={}
-    #print(ulke_basina)
+    ulke_toplam={}
+    ulke_toplam_arr=[]
     for i in ulke_basina:
         ulke_sayac[i]=[]
+        if not i in ulke_toplam:
+            ulke_toplam[i]=[i,0,0]
         for j in ulke_basina[i]:
-            ulke_sayac[i].append([j,ulke_basina[i][j][0],ulke_basina[i][j][1],max(0,ulke_basina[i][j][1]-1)/(ulke_basina[i][j][0]+ulke_basina[i][j][1])])
+            ulke_sayac[i].append([j,ulke_basina[i][j][0],ulke_basina[i][j][1],oranla(ulke_basina[i][j][1],ulke_basina[i][j][0])])
+            ulke_toplam[i]=[i,ulke_toplam[i][1]+ulke_basina[i][j][0],ulke_toplam[i][2]+ulke_basina[i][j][1]]
         ulke_sayac[i].sort(key=lambda a:a[2],reverse=True)
+    for i in ulke_toplam:
+        ulke_toplam[i].append(oranla(ulke_toplam[i][2],ulke_toplam[i][1]))
+        ulke_toplam_arr.append(ulke_toplam[i])
+    ulke_toplam_arr.sort(key=lambda a:a[3],reverse=True)
+    print(ulke_toplam_arr)
     baskentler=pd.read_csv("./data/countries.csv")
     baskent_dict={}
     for _,i in baskentler.iterrows():
@@ -76,5 +96,58 @@ if __name__ == '__main__':
     print(xs,ys,zs)
     ax.scatter(xs, ys, zs)
     ax.set_yticks(ys,[i[0] for i in gecerli_ulke_adet])
+    #plt.show()
+    hafta_sayilar={}
+    ulke_gecerli_gun_miktar={}
+    guncel_hafta=veri_gunler[0].strftime("%Y-%m-%d")
+    hafta_sayilar[guncel_hafta]={}
+    hafta_oranlar={"date":[]}
+    son_tarih=veri_gunler[0].timestamp()
+    bir_hafta=7*24*60*60
+    for i in range(miktar):
+        tarih=veri_gunler[i]
+        guncel_timestamp=tarih.timestamp()
+        print(guncel_timestamp-son_tarih)
+        while guncel_timestamp-son_tarih>bir_hafta:
+            son_tarih+=bir_hafta
+            yeni_hafta=datetime.datetime.fromtimestamp(son_tarih).strftime("%Y-%m-%d")
+            print(yeni_hafta,guncel_hafta)
+            guncel_hafta=yeni_hafta
+            hafta_sayilar[guncel_hafta]={}
+        veri=gunluk_ulke_sayac[i]
+        for j in veri:
+            if not j in hafta_sayilar[guncel_hafta]:
+                hafta_sayilar[guncel_hafta][j]=[0,0]
+            if not j in ulke_gecerli_gun_miktar:ulke_gecerli_gun_miktar[j]=0
+            ulke_gecerli_gun_miktar[j]+=1
+            hafta_sayilar[guncel_hafta][j]=[hafta_sayilar[guncel_hafta][j][0]+veri[j][0],hafta_sayilar[guncel_hafta][j][1]+veri[j][1]]
 
-plt.show()
+    print("hafta sayılar",hafta_sayilar)
+    to_delete=[]
+    for i in hafta_sayilar:
+        for j in hafta_sayilar[i]:
+            if ulke_gecerli_gun_miktar[j]!=miktar: to_delete.append([i,j])
+    for i in to_delete:del hafta_sayilar[i[0]][i[1]]
+    for i in hafta_sayilar:
+        hafta_oranlar["date"].append(i)
+        for j in hafta_sayilar[i]:
+            deger=math.floor(1000*oranla(hafta_sayilar[i][j][1],hafta_sayilar[i][j][0]))/10
+            if not j in hafta_oranlar: hafta_oranlar[j]=[]
+            hafta_oranlar[j].append(deger)
+
+    hafta_df=pd.DataFrame.from_dict(hafta_oranlar)
+    hafta_df.set_index("date",inplace=True)
+
+    print(hafta_df)
+    bcr.bar_chart_race(
+        df=hafta_df,
+        filename="test.mp4",
+        orientation='h', 
+        sort='desc', 
+        n_bars=10, 
+        bar_texttemplate='{x:,.2f}',
+        period_length=2000,
+        steps_per_period=50,
+        end_period_pause=500,
+        title="Haftalık GPS Jammer Sıralaması"
+    ) 
--- veriler.py
+++ veriler.py
@@ -3,15 +3,20 @@ import requests
 from datetime import timedelta,date,datetime
 gun=timedelta(days=1)
 guncel=datetime.now()-gun*2
-sayac=len(os.listdir("./data"))
+sayac=len(os.listdir("./data/map"))
+sorun_sayac=0
 while sayac<600:
     tarih=guncel.strftime("%Y-%m-%d")
     link="https://gpsjam.org/data/"+tarih+"-h3_4.csv"
     r=requests.get(link)
     guncel-=gun
-    if r.status_code!=200:continue
+    if r.status_code!=200:
+        sorun_sayac+=1
+        if sorun_sayac==7:break
+        continue
+    sorun_sayac=0
     print(sayac,guncel.strftime("%Y-%m-%d"))
-    f = open("./data/"+tarih+".csv", "w")
+    f = open("./data/map/"+tarih+".csv", "w")
     f.write(r.text)
     f.close()
     sayac+=1
